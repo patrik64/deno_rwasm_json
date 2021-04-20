73149caae68e157003fe58e5374dbb8c7638b47b
Patrik Simic <patrik.simic@gmail.com>
2021-04-21 00:27:31 +0200

    using rust format

--- src/lib.rs
+++ src/lib.rs
@@ -14,7 +14,7 @@ extern "C" {
     fn log(s: &str);
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 struct Address {
     number: Option<u32>,
     street: String,
@@ -48,7 +48,10 @@ pub fn get_json() -> String {
 #[wasm_bindgen]
 pub fn set_json(json_string_input: &str) {
     let address: Address = serde_json::from_str(&json_string_input).unwrap();
+    let fmt_address = format!("{:?}", &address);
+    log("json received from deno as a string, deserialized to a struct and printed from within rust: ");
+    log(&fmt_address);
     let str = serde_json::to_string(&address).unwrap();
-    log("json received from deno and printed from within rust: ");
+    log("the same rust struct serialized and printed from within rust: ");
     log(&str);
 }
